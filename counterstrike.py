4fda7e6257930e56ffaed0a8edca8063ccca166d
Rasmus <rasmustofthermansen@gmail.com>
2024-05-02 22:15:39 +0200

    Counter strike matches update

--- counterstrike.py
+++ counterstrike.py
@@ -4,6 +4,30 @@ import aiohttp
 from share import *
 from bs4 import BeautifulSoup
 import locale
+ 
 
-load_dotenv() 
-CS_matches_for_the_day =[]
+from selenium import webdriver
+
+async def scrape_matches_cs():
+    url = "https://bo3.gg/matches/current"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            text = await response.text()
+    soup = BeautifulSoup(text, 'html.parser')
+    mydate_time = datetime.datetime.now()
+    mydate = mydate_time.date()
+    mydate_string = str(mydate)
+    matches = soup.find_all('div', class_='table-cell match')
+    for match in matches:
+        date = match.get('date')
+        teamnames = match.find_all('div', class_='team-name')
+        ko= str(date)
+        hest = ko[0:10]
+        hund = ko[11:16]
+        if  teamnames:
+            if mydate_string == hest:
+                firstteam = teamnames[0].text.strip()
+                secondteam = teamnames[1].text.strip()
+                print(f"Team : {firstteam}   VS    Team : {secondteam}        time: {hund} ")
+            
+asyncio.run(scrape_matches_cs())
--- share.py
+++ share.py
@@ -3,3 +3,7 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 from dotenv import load_dotenv
 import os
+import asyncio
+
+
+    
