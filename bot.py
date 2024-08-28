b73204fc218128d0549cf26b65517db9019f0852
Rasmus <rasmustofthermansen@gmail.com>
2024-08-28 21:18:42 +0200

    Make ufc working again

--- bot.py
+++ bot.py
@@ -24,7 +24,12 @@ async def f1(ctx):
    
 @client.command()
 async def cs(ctx):
-    await Counterstrike.scrape_matches()
+    matches_info = Counterstrike.hello()
+    if matches_info:
+        await ctx.send(f"**Live Matches:**\n\n{matches_info}")
+    else:
+        await ctx.send("No live matches at the moment.")
+        
 @client.command()
 async def ufc(ctx):
     await ufc.scrape_matches()
--- ufc.py
+++ ufc.py
@@ -9,14 +9,13 @@ async def scrape_matches():
             text = await response.text()
     soup = BeautifulSoup(text, 'html.parser')
     locale.setlocale(locale.LC_TIME, "da_DK.UTF-8") 
-    matches = soup.find_all('li', class_='l-listing__item')
-    
+    matches = soup.find_all('div', class_='l-listing__item views-row')
     # Define time zones for GMT+2 (CEST) and Copenhagen
     edt_timezone = pytz.timezone('America/New_York')
     copenhagen_timezone = pytz.timezone('Europe/Copenhagen')
-    
     for match in matches:
         head_line = match.find_all('h3', class_= 'c-card-event--result__headline')
+        
         match_info = match.find_all('div', class_='c-card-event--result__date tz-change-data')
         fighters = head_line[0].text.strip()
         stripped_match_info = match_info[0].text.strip()
@@ -26,14 +25,15 @@ async def scrape_matches():
         time = split_match_info[4]
         time_zone = split_match_info[5]
         mma_kamp_cest = month + " " + day + " " + time + " " + time_zone
+        print
         race_date_1 = mma_kamp_cest.replace("PM", "").replace("May", "Maj").replace("Oct","Okt")
         cest_time = datetime.strptime(race_date_1, "%b %d %I:%M %p")
         cest_time = edt_timezone.localize(cest_time)
         copenhagen_time = cest_time.astimezone(copenhagen_timezone)
         mma_kamp_copenhagen = copenhagen_time.strftime("%d %B     %I:%M")
         matches_for_the_day.append(f"**Date and time:**\t {mma_kamp_copenhagen}\n**Headline:**\t {fighters}\n{'-'*60}\n ")
-        #print(f"**Date and time:**\t {mma_kamp_copenhagen}\n**Headline:**\t {fighters}\n{'-'*60}\n ")
-        #print(f"**Date**\t {mma_kamp_copenhagen}\n**Headline:**\t {fighters}\n{'-'*60}\n ")
+        # print(f"**Date and time:**\t {mma_kamp_copenhagen}\n**Headline:**\t {fighters}\n{'-'*60}\n ")
+        # print(f"**Date**\t {mma_kamp_copenhagen}\n**Headline:**\t {fighters}\n{'-'*60}\n ")
         
     channel = client.get_channel(1234600336291790980)
     await channel.purge(limit=5)
