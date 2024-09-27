7073cb2001a33dcd116846724947181f96935408
Rasmus <rasmustofthermansen@gmail.com>
2024-09-27 15:56:01 +0200

    Added more functions to existing code

--- bot.py
+++ bot.py
@@ -31,18 +31,27 @@ async def UFC(ctx):
 async def clear(ctx, amount = 50):
     await ctx.channel.purge(limit=amount)
     
+    
+def wait_until_ready():
+    start_football_loop.before_loop(client.wait_until_ready)
+    start_f1_loop.before_loop(client.wait_until_ready)
+    start_Ufc_loop.before_loop(client.wait_until_ready)
+    start_counterstrike_loop.before_loop(client.wait_until_ready)
+    twitch_loop.before_loop(client.wait_until_ready)
+
+def loop_start():
+    start_football_loop.start()
+    start_Ufc_loop.start()
+    start_f1_loop.start()
+    start_counterstrike_loop.start()
+    twitch_loop.start()
+    
+    
 @client.event
 async def on_ready():
-   start_football_loop.before_loop(client.wait_until_ready)
-   start_f1_loop.before_loop(client.wait_until_ready)
-   start_Ufc_loop.before_loop(client.wait_until_ready)
-   start_counterstrike_loop.before_loop(client.wait_until_ready)
-   twitch_loop.before_loop(client.wait_until_ready)
-   start_football_loop.start()
-   start_Ufc_loop.start()
-   start_f1_loop.start()
-   start_counterstrike_loop.start()
-   twitch_loop.start()
+    wait_until_ready()
+    loop_start()
+   
             
 def main():
 
