fab696233636b1ee563861182edc6dd6236866f2
Rasmus <rasmustofthermansen@gmail.com>
2024-10-11 22:37:24 +0200

    github try

--- F1.py
+++ F1.py
@@ -44,7 +44,6 @@ async def scrape_matches():
                         if i % 5== 0:
                             #print(f"**Racename:** {race_name_split}\n{'-'*60}\n")
                             matches_for_the_day.append(f"**Racename:** {race_name_split}\n{'-'*60}\n")
-
                 i = i + 1
             return(True)
         
