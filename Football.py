4bee351af50b2de6a2e8aa298ef4ba55a2ca4a9d
Rasmus <rasmustofthermansen@gmail.com>
2024-12-03 14:33:51 +0100

    Football

--- Football.py
+++ Football.py
@@ -10,50 +10,33 @@ matches_for_the_day =[]
 
 
 async def scrape_matches():
-    url = fodbold_URL
+    i =1
+    url = "https://www.livescore.dk/fodbold-i-tv/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             text = await response.text()
     soup = BeautifulSoup(text, 'html.parser')
-    locale.setlocale(locale.LC_TIME, "da_DK.UTF-8") 
-    mydate = datetime.datetime.now()
-    formatted_date = mydate.strftime("%e. %b")
-    format_dt = formatted_date.split(" ")
-    try:
-        spiltted_date = format_dt[1] + " " +  format_dt[2]
-    except:
-         spiltted_date = format_dt[0] + " " +  format_dt[1]
-    ongoing_time = mydate.strftime("%H:%M")
-    matches = soup.find_all('li', class_=matche)
-    for match in matches:
-        teams = match.find('div', class_='teams').text.strip()
-        split_teams = teams.split("  ")
-        formattted_teams = split_teams[0] + "\t VS \t" + split_teams[1]
-        time = match.find('div', class_='date-time-wrapper').text.strip()
-        formatted_time = time.split(" ");
-        matchtime = formatted_time[0] + " " + formatted_time[1]
-        find_league = match.find_all('small', class_='text-secondary d-none d-sm-inline-block mx-2')
-        league = find_league[0].text.strip()
-        if(spiltted_date == matchtime):
-            if ongoing_time > formatted_time[2]:
-                status = "Ongoing"
-                
-            elif ongoing_time < formatted_time[2]:
-                status = "Upcoming"
-                
-            #print(f"**Teams: ** {formattted_teams}\n**Time: ** {formatted_time[2]}\n**League: ** {league}\n**Status: ** {status}\n{'-'*60}\n")
-            matches_for_the_day.append(f"**Teams:** {formattted_teams}\n**Time:** {formatted_time[2]}\n**League:** {league}\n**Status:** {status}\n{'-'*60}\n")
 
-    channel = client.get_channel(1234600317090529392)
-    await channel.purge(limit=5)
-    if matches_for_the_day:
-        matches_message = "\n".join( matches_for_the_day)
-        await channel.send("<@&1234890120029536297>")
-        await channel.send("**Todays matches:**")
-        for part in split_message(matches_message.split("\n")):
-            await channel.send(part)
-        matches_for_the_day.clear()
-        return True
-    else:
-        await channel.send("No matches for today.")
-        return False
+    header = soup.find_all('div', class_='event')
+    for match in header:
+        time = match.find('div', class_='time').text.strip()
+        teams = match.find('span', class_='text').text.strip()
+        league = match.find('div', class_='league').text.strip()
+        i += i
+        matches_for_the_day.append(f"**Teams:** {teams} \n**League:** {league}\n**Time:** {time}\n{'-'*60}\n ")
+        if(i ==7):
+           
+            channel = client.get_channel(1234600317090529392)
+            await channel.purge(limit=5)
+            if matches_for_the_day:
+                matches_message = "\n".join( matches_for_the_day)
+                await channel.send("<@&1234890120029536297>")     
+                await channel.send("**Todays matches:**")
+                for part in split_message(matches_message.split("\n")):
+                    await channel.send(part)
+                matches_for_the_day.clear()
+                return True
+            else:
+                await channel.send("No matches for today.")
+                return False
+            
