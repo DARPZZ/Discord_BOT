4cdadf5404b0287445ad1916b31ab704568246f4
Darpzz <rasmustofthermansen@gmail.com>
2025-02-13 15:13:37 +0100

    - Updated football to only take the matches that is on today.
    -Disabeld the f1 until we get closer to season start

--- Football.py
+++ Football.py
@@ -2,41 +2,42 @@ import datetime
 import aiohttp
 from share import *
 from discord.ext import tasks
-load_dotenv() 
-fodbold_URL = os.getenv("fodbold_URL")
-matche = os.getenv("matches")
 matches_for_the_day =[]
 
 
 
 async def scrape_matches():
-    i =1
+    loop_bool = True
     url = "https://www.livescore.dk/fodbold-i-tv/"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             text = await response.text()
     soup = BeautifulSoup(text, 'html.parser')
-
-    header = soup.find_all('div', class_='event')
-    for match in header:
-        time = match.find('div', class_='time').text.strip()
-        teams = match.find('span', class_='text').text.strip()
-        league = match.find('div', class_='league').text.strip()
-        i += 1
-        matches_for_the_day.append(f"**Teams:** {teams} \n**League:** {league}\n**Time:** {time}\n{'-'*60}\n ")
-        if(i ==7):
-           
-            channel = client.get_channel(1234600317090529392)
-            await channel.purge(limit=5)
-            if matches_for_the_day:
-                matches_message = "\n".join( matches_for_the_day)
-                await channel.send("<@&1234890120029536297>")     
-                await channel.send("**Todays matches:**")
-                for part in split_message(matches_message.split("\n")):
-                    await channel.send(part)
-                matches_for_the_day.clear()
-                return True
-            else:
-                await channel.send("No matches for today.")
-                return False
+    todays_matches = soup.find('div', class_='tv-table tv-table--football')
+    football_match = todays_matches.find_next()
+    while loop_bool:
+        match = football_match.find_next_sibling()
+        football_match = match
+        if match.find('div', class_='date'):
+            loop_bool = False
+            break
+        teams = football_match.find('span', class_='text').text.strip()
+        tid = football_match.find('div', class_='time').text.strip()
+        kanal = football_match.find('div', class_='chanels')
+        img_tag = kanal.find('img')
+        kanal = img_tag.get('alt', 'No alt attribute') if img_tag else 'No image found'
+        league = football_match.find('div', class_='league')
+        real_league = league.find('span', class_='text').text.strip()
+        matches_for_the_day.append(f"**Teams:** {teams} \n**Tid:** {tid}\n**Liga:** {real_league} \n**Kanal:** {kanal} \n{'-'*60}\n ")
+    channel = client.get_channel(1234600317090529392)
+    await channel.purge(limit=25)
+    if matches_for_the_day:
+        matches_message = "\n".join(matches_for_the_day)
+        matches_for_the_day.clear()
+        await channel.send("<@&1234866967630839870>")
+        await channel.send("**Today's matches:**")
+        for part in split_message(matches_message.split("\n")):
+            await channel.send(part)
+    else:
+        await channel.send("No matches for today.")
             
