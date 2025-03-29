c54cc16d1a3a105fa50ccae91e6c1ed2963ac95d
Darpzz <rasmustofthermansen@gmail.com>
2025-03-29 16:37:44 +0100

    rerun

--- CounterStrike.py
+++ CounterStrike.py
@@ -53,9 +53,8 @@ async def scrape_matches():
                     else:
                         bo_type_stripped = "Unknown"
                     time_object = datetime.strptime(match_time, "%H:%M")
-
+                    time_object += timedelta(hours=2)
                     new_time_string = time_object.strftime("%H:%M")
-                    new_time_string += timedelta(hours=1)
                     embedVar.add_field(name="**Teams:**", value=get_team_names(table_row), inline=False)
                     embedVar.add_field( name="**Time:**", value=new_time_string, inline=False)
                     embedVar.add_field(name="**BO:**", value=bo_type_stripped,inline=False)
