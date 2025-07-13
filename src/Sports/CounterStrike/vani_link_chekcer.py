284ed29d536ef3bf6eea8055cdf2f0a9f6267374
Darpzz <rasmustofthermansen@gmail.com>
2025-07-13 20:22:31 +0200

    Remove suffix if you copy link from steam

--- src/Sports/CounterStrike/vani_link_chekcer.py
+++ src/Sports/CounterStrike/vani_link_chekcer.py
@@ -1,5 +1,9 @@
 import src.Sports.CounterStrike.GetPlayerInfo.steam_api_call as checker
+
+
 async def check_if_link_is_64(PlayerID):
+    if(PlayerID[-1] == "/"):
+        PlayerID = PlayerID[:-1]
     splittede_player_id =""
     if("id/" in PlayerID):
        splittede_player_id = PlayerID.split("id/")
@@ -8,8 +12,10 @@ async def check_if_link_is_64(PlayerID):
     user_stats_data = await checker.CheckForVanityLink(splittede_player_id[1])
     link_list = user_stats_data['response']
     is_a_vanity_link = link_list.get("success")
+    
     if(is_a_vanity_link == 1):
         return link_list.get("steamid")
     else:
         return splittede_player_id[1]
+
     
