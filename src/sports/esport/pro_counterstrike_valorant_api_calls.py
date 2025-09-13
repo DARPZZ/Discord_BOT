a686d34e51f770dbeeeeef9ebbdcf44e9d949b92
Rasmustofth <rasmustofthermansen@gmail.com>
2025-09-13 10:52:41 +0200

    Tournament info for counter strike

--- src/sports/esport/pro_counterstrike_valorant_api_calls.py
+++ src/sports/esport/pro_counterstrike_valorant_api_calls.py
@@ -44,5 +44,18 @@ async def get_counter_strike_valorant_stream_coverage(slug):
     except requests.exceptions.RequestException as e:
         print('Error:', e)
         return None
-    
+
+
+async def get_counter_strike_upcomming_tournaments():
+    url = f"https://api.bo3.gg/api/v1/tournaments?scope=index-current-tournaments&page%5Boffset%5D=0&page%5Blimit%5D=50&sort=start_date&filter%5Btournaments.status%5D%5Bin%5D=current,upcoming&filter%5Btournaments.discipline_id%5D%5Beq%5D=1&with=teams"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            matches = response.json()
+            return matches
+        else:
+            print('Error:', response.status_code)
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
 
