fc8c88f41cfd6e6cc34f1f74e3a9f9f76c6d01b2
Rasmustofth <rasmustofthermansen@gmail.com>
2025-09-23 18:46:59 +0200

    Removed the error message from counter strike where 1 of 2 could trigger a error message. Now they need to both trigger to send error message

--- src/sports/esport/counterstrike/counter_strike.py
+++ src/sports/esport/counterstrike/counter_strike.py
@@ -5,16 +5,15 @@ import src.sports.esport.counterstrike.create_counter_strike_embeds as  create_c
 import src.sports.esport.pro_esport as pro_esport
 from src.get_settings import read_settings_file as settings
 APIID = 1
+total_matches = []
 tournament_dict = {}
-ERROR_UPCOMMING_MATCHES = "We could not find any high tier matches, that are upcomming"
-ERROR_LIVE_MATCHES = "We could not find any high tier matches, that are live"
+ERROR_MESSAGE = "We could not find any high tier matches, that are upcomming or live"
 async def show_info_for_upcomming_matches(channel):
     try:
         data = await api_call.get_counter_strike_valorant_pro_info_upcomming(APIID)
         if data:
             match_data = data['data']['tiers']['high_tier']['matches']
             if(match_data is None):
-                await channel.send(ERROR_UPCOMMING_MATCHES)
                 return 
             for element in match_data:
                 stars = element.get('stars')
@@ -33,12 +32,12 @@ async def show_info_for_upcomming_matches(channel):
                 create_counter_strike_embeds.create_upcomming_matches_enmed(
                     embedVar, team_names, odds, bo_type, tournament_info[0], tournament_info[1]
                 )
+                total_matches.append(embedVar)
                 
                 await create_counter_strike_embeds.create_streams_embed(streams, embedVar)
                 await channel.send(embed=embedVar)
     except Exception as e:
         print(f"Error in show_info_for_upcomming_matches: {e}")
-        await channel.send(ERROR_UPCOMMING_MATCHES)
 
 
 async def show_info_for_live_matches(channel):
@@ -47,10 +46,10 @@ async def show_info_for_live_matches(channel):
         if(data):
             match_data = data['data']
             if(match_data is None):
-                await channel.send(ERROR_UPCOMMING_MATCHES) 
+                return
             for element in match_data:
                 stars = element.get('stars',0)
-                if (stars < 2):
+                if (stars < 5):
                     continue
                 maps = pro_esport.get_maps(element)
                 tournament_info = await pro_esport.place_tournament_info(element,data,tournament_dict)
@@ -59,23 +58,27 @@ async def show_info_for_live_matches(channel):
                 team1_score = element.get('team1_score')
                 team2_score =element.get('team2_score')
                 create_counter_strike_embeds.create_live_matches_enmed(embedVar,team_names,team1_score,team2_score,tournament_info[0],tournament_info[1])
-                
                 embedVar.add_field(name=f"",value="**Maps: **",inline=False)
                 maps =  pro_esport.get_maps(element)
                 for mapp in maps:
                     embedVar.add_field(name=f"",value=mapp,inline=False)
                 slug = element.get('slug')
                 streams = await pro_esport.get_stream_coverage(slug,api_call)
+                total_matches.append(embedVar)
                 await create_counter_strike_embeds.create_streams_embed(streams,embedVar)
                 await channel.send(embed=embedVar)
     except Exception as e:
         print(f"Error in show_info_for_live_matches: {e}")
-        await channel.send(ERROR_LIVE_MATCHES)        
+   
 
 async def show_info():
-    
     channel = client.get_channel(settings("proPlayIDCs"))
     await channel.purge(limit=50)
     await show_info_for_live_matches(channel)
     await show_info_for_upcomming_matches(channel)
+    if(len(total_matches) <= 0):
+       embedVar = discord.Embed( color=0x9DFF00)
+       embedVar.add_field(name="",value=ERROR_MESSAGE,inline=False)
+       await channel.send(embed=embedVar)
+    total_matches.clear()
     return True
