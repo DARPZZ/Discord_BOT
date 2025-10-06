c2ab7f1b1c4602418b76b7e541484164d1268ea8
Rasmustofth <rasmustofthermansen@gmail.com>
2025-10-06 18:29:59 +0200

    Moved things out of bot.py into diffrent files for better controle

--- bot.py
+++ bot.py
@@ -1,13 +1,9 @@
 from share import *
-from src.sports.football import football
 import reaction_role
-from discord import app_commands
 from loop import *
-import src.sports.esport.counterstrike.vani_link_chekcer as link_checker
-import src.sports.esport.counterstrike.GetPlayerInfo.cs2_match_stats as cs2_match_stats
-import src.sports.f1.f1 as formula1
-from src.logs import get_logs
-from src.get_settings import read_settings_file as settings
+import src.commands.counterstrike_commands as counterstrike_commands
+import src.commands.essentials as essentials
+import src.commands.loop_commands as loop_commands
 load_dotenv() 
 discord_token = os.getenv("discord_token")
 intents.message_content = True
@@ -22,89 +18,8 @@ async def on_ready():
     except Exception as e:
         print(f"Failed to sync commands: {e}")
     await loop_start()
-   
-
-#region sportsCommands
-def has_owner_role(interaction: discord.Interaction) -> bool:
-    return any(role.name.lower() == "owner" for role in interaction.user.roles)
-
-@client.tree.command(name="football", description="Scrape football matches")
-async def football(interaction: discord.Interaction):
-    if not has_owner_role(interaction):
-        return
-    await interaction.response.send_message("Scraping football matches...")
-    await start_football_loop.start()
-
-
-@client.tree.command(name="cs", description="Scrape Counter-Strike matches")
-async def cs(interaction: discord.Interaction):
-    if not has_owner_role(interaction):
-        return
-    await interaction.response.send_message("Scrape Counter-Strike matches...")
-    await start_counterstrike_loop.start()
-
-@client.tree.command(name="f1", description="Scrape F1 matches")
-async def f1(interaction: discord.Interaction):
-    if not has_owner_role(interaction):
-        return
-    await interaction.response.send_message("Scraping F1 matches...")
-    await start_f1_loop.start()
-
-@client.tree.command(name="clear", description="Clear a number of messages from the channel")
-@app_commands.describe(amount="Number of messages to delete (default 50)")
-async def clear(interaction: discord.Interaction, amount: int = 50):
-    if not has_owner_role(interaction):
-        return
-    await interaction.response.send_message(f"Clearing {amount} messages...")
-    await interaction.channel.purge(limit=amount)
-
-@client.tree.command(name="logs", description="Get logs")
-
-async def logs(interaction: discord.Interaction):
-    if not has_owner_role(interaction):
-        return
-    allowed_channel_id = settings("logsID")  
-    if interaction.channel.id != allowed_channel_id:
-        await interaction.response.send_message(
-            "❌", 
-            ephemeral=True
-        )
-        return
-    await interaction.response.send_message(f"getting logs",delete_after=1)
-    await get_logs()
     
-async def get_numbers_of_ID(accounts,interaction):
-    if(len(accounts)>10):
-        await interaction.response.send_message("You can´t have that many id´s", ephemeral=True)
-        return False
-    else:
-        return True
     
-
-@client.tree.command(name="csstats", description="Get information about cs2 players")
-@app_commands.describe(player_id="Players id")
-async def csstats(interaction: discord.Interaction, player_id: str):
-    allowed_channel_id = settings("playerLookUpID")  
-    
-    if interaction.channel.id != allowed_channel_id:
-        await interaction.response.send_message(
-            "❌ This command can only be used in the designated channel: " +"cs2-player-lookup", 
-            ephemeral=True
-        )
-        return
-    accounts =  player_id.split(" ")
-    if(await get_numbers_of_ID(accounts,interaction)):
-        await interaction.response.send_message("Getting data...", ephemeral=True)
-        for element in accounts:
-            player_id_after_vanity = await link_checker.check_if_link_is_64(element)
-            if(player_id_after_vanity == None):
-                await interaction.followup.send("Enter a valid steam ID",ephemeral=True)
-                return
-            
-            data = await cs2_match_stats.get_info(player_id_after_vanity)
-            await interaction.followup.send(embed=data, ephemeral=True)
-#endregion
-
 @client.event
 async def on_member_join(member): 
   role = discord.utils.get(member.guild.roles, name="Medlem")
