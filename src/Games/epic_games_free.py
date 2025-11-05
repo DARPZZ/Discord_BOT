02012763d301e6890eca0abed4c18db3a0318dbb
Rasmustofth <rasmustofthermansen@gmail.com>
2025-11-05 03:10:02 +0100

    Add so it shows the live games first and upcomming after
    add test for epic games

--- src/Games/epic_games_free.py
+++ src/Games/epic_games_free.py
@@ -3,17 +3,19 @@ import datetime
 from share import *
 from src.Games.epic_games_api import get_data
 class epic_games:
+    
     def __init__(self,settings):
         self.settings = settings
         self.channel = client.get_channel(self.settings("epic_games"))
+        self.live_embds_list =[]
+        self.upcomming_embds_list=[]
+        
     def convert_date(self,date):
         clean_date = date.rstrip('Z') 
         dt = datetime.datetime.fromisoformat(clean_date)
         converted_date = dt.strftime("%d - %m")
         return converted_date
     
-   
-        
     async def check_promo_data(self,elements):
         LIVE_GAMES = "promotionalOffers"
         LIVE_GAMES_TEXT = "**Live Games**"
@@ -27,10 +29,14 @@ class epic_games:
             if promo == None:
                 continue
             if(promo.get("promotionalOffers") != []):
-                await self.offers(promo,title,offerType,img,LIVE_GAMES,LIVE_GAMES_TEXT)
+                embed_final = await self.offers(promo,title,offerType,img,LIVE_GAMES,LIVE_GAMES_TEXT)
+                if embed_final != None:
+                    self.live_embds_list.append(embed_final)
             else:
-                await self.offers(promo,title,offerType,img,UPCOMMING_GAMES,UPCOMMING_GAMES_TEXT)
-   
+                embed_final =await self.offers(promo,title,offerType,img,UPCOMMING_GAMES,UPCOMMING_GAMES_TEXT)
+                if embed_final != None:
+                    self.upcomming_embds_list.append(embed_final)
+                
     async def offers(self,promo,title,offerType,img,version,embed_title):
         PromotionalOffers = promo.get(version)
         for x in PromotionalOffers:
@@ -47,7 +53,7 @@ class epic_games:
                 title = title
                 offerType = offerType
                 img = img
-                await self.create_discord_embed(
+                embed = await self.create_discord_embed(
                     title,
                     converted_start_date,
                     converted_end_date,
@@ -55,6 +61,8 @@ class epic_games:
                     embed_title,
                     img
                 )
+                return embed
+            
     async def create_discord_embed(self,title,start_date_converted,end_date_converted,offertype,type,img_url):
         embedVar_test = discord.Embed( color=0x9D00FF)
         embedVar_test.title = type
@@ -63,10 +71,12 @@ class epic_games:
         embedVar_test.add_field(name="**Slut Dato:** ",value=end_date_converted,inline=False)
         embedVar_test.add_field(name="**offertype:** ",value=offertype,inline=False)
         embedVar_test.set_image(url=img_url)
-        await self.channel.send(embed=embedVar_test)
+        return embedVar_test
         
 
     async def get_free_games_on_epic_games(self):
+        self.upcomming_embds_list.clear()
+        self.live_embds_list.clear()
         data = await get_data()
         await self.channel.purge()
         if not data:
@@ -78,3 +88,5 @@ class epic_games:
                 .get("elements", [])
         )
         await self.check_promo_data(elements)
+        await self.channel.send(embeds=self.live_embds_list)
+        await self.channel.send(embeds=self.upcomming_embds_list)
